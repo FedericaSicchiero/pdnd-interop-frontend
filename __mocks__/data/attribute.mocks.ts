cf9e3a0a59c78b0a9b6ac4e8d840f7930645a232
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-04-14 16:40:35 +0200

    Added AgreementDetails tests

--- __mocks__/data/attribute.mocks.ts
+++ __mocks__/data/attribute.mocks.ts
@@ -1,4 +1,4 @@
-import type { EServiceAttribute, EServiceAttributeValue } from '@/api/api.generatedTypes'
+import type { Attribute, EServiceAttribute, EServiceAttributeValue } from '@/api/api.generatedTypes'
 import type { PartyAttribute } from '../../src/types/attribute.types'
 import { createMockFactory } from '../../src/utils/testing.utils'
 
@@ -6,6 +6,14 @@ const createMockPartyAttribute = createMockFactory<PartyAttribute>({
   id: 'id-party-attribute',
   name: 'Attribute Name',
   state: 'ACTIVE',
+})
+
+const createMockAttribute = createMockFactory<Attribute>({
+  id: 'id-party-attribute',
+  name: 'Attribute Name',
+  description: 'Attribute description',
+  kind: 'CERTIFIED',
+  creationTime: '2021-09-01T12:00:00.000Z',
 })
 
 const createMockBackendAttributeContent = createMockFactory<EServiceAttributeValue>({
@@ -30,4 +38,5 @@ export {
   createMockPartyAttribute,
   createMockSingleBackendAttribute,
   createMockGroupBackendAttribute,
+  createMockAttribute,
 }
